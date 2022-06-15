aa121bc8b18146b519cd60d41c1c95ff77a5b585
SeoWo0 <97532242+SeoWo0@users.noreply.github.com>
2022-06-15 18:37:33 +0900

    UI 구현
    Login
    InConnect
    CreateRoom
    Lobby
    Info 각각의 Panel로 기본적 UI 구현 ing

---
+++ Assets/Scripts/Manager/LobbyManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LobbyManager : MonoBehaviour
+{
+    public static LobbyManager instance { get; private set;}
+    
+    [Header("Panel")]
+
+    private void Awake() {
+        instance = this;
+    }
+}

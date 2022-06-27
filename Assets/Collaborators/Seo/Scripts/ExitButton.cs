07e5f716a176ab26541f57e890fb6fefeb7a1bcc
SeoWo0 <97532242+SeoWo0@users.noreply.github.com>
2022-06-27 15:43:12 +0900

    Fix : UI Convenience

---
+++ Assets/Collaborators/Seo/Scripts/ExitButton.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class ExitButton : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject targetWindow;
+
+    private void Update() {
+
+        if(Input.GetKeyDown(KeyCode.Escape))
+        {
+            targetWindow.SetActive(false);
+        }
+    }
+}

04bf7262303bb1051cd3f131cec91b20bc9aaba7
SeoWo0 <97532242+SeoWo0@users.noreply.github.com>
2022-06-27 19:01:52 +0900

    Fix : Make 2 Player prefabs

--- Assets/Scripts/Game/GameManager.cs
+++ Assets/Scripts/Game/GameManager.cs
@@ -103,7 +103,7 @@ public class GameManager : Singleton<GameManager>
 
                 break;
             case 1:
-                GameObject playerModel2 =PhotonNetwork.Instantiate("NewPlayer", spawnPos[playerNumber].position, spawnPos[playerNumber].rotation, 0);
+                GameObject playerModel2 =PhotonNetwork.Instantiate("Player 2", spawnPos[playerNumber].position, spawnPos[playerNumber].rotation, 0);
                 player = playerModel2.GetComponent<PlayerMove>();
                 break;
         }

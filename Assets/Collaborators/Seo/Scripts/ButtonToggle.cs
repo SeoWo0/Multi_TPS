ed76354029e466c28adabe36b25ff38129398390
RAGG <50361901+r4skan@users.noreply.github.com>
2022-06-29 19:29:19 +0900

    Feat : TitleScene Update, ButtonAnimation, Volume Setting

---
+++ Assets/Collaborators/Seo/Scripts/ButtonToggle.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonToggle : MonoBehaviour
+{
+    public GameObject targetObj;
+
+    private void LateUpdate()
+    {
+        if (Input.GetButtonDown("Cancel") && targetObj.activeSelf)
+        {
+            targetObj.SetActive(false);
+        }
+    }
+
+    public void ObjectToggle()
+    {
+        targetObj.SetActive(!targetObj.activeSelf);
+    }
+}
